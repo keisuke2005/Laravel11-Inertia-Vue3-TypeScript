de2aef1e5c872f523d17609507c4567c36d94ccc
keisuke.ueda <ukei2021@gmail.com>
2024-05-14 01:51:11 +0900

    修正

--- init.sh
+++ init.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
-cd /var/www/html && composer install --no-interaction --optimize-autoloader --no-dev
-cd /var/www/html && php artisan key:generate
-cd /var/www/html && php artisan config:cache
-cd /var/www/html && php artisan migrate --force
-cd /var/www/html && php artisan db:seed --force
+
+cd /var/www/html
+
+# Composerの依存関係をインストール
+composer install
+
+# 環境設定ファイルをコピー
+cp -fp .env.local .env
+
+chown -R application:application ./*
+chmod -R ug+rwx storage bootstrap/cache 
+
+
+# アプリケーションキーを生成
+php artisan key:generate
+
+# 設定をキャッシュ
+php artisan config:cache
+
+# データベースマイグレーション
+php artisan migrate --force
+
+# データベースシーディング
+php artisan db:seed --force
+
+php artisan config:clear
